421de312f7c491989c4599fc0b16d4c1aa6ea0e7
Nicolas-Bessin <nicolasbessin49@gmail.com>
2024-07-26 14:04:45 +0200

    Column generation as it's own function - allows for multiple rounds easier

---
+++ src/column_generation.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "instance.h"
+#include "solution.h"
+#include "pricing.h"
+#include <vector>
+
+struct CGResult {
+    MasterSolution master_solution;
+    IntegerSolution integer_solution;
+    std::vector<Route> routes;
+    int number_of_iterations;
+    int master_time;
+    int pricing_time;
+    int integer_time;
+};
+
+
+CGResult column_generation(const Instance & instance, std::vector<Route> initial_routes, double reduced_cost_threshold, int time_limit = 60, bool verbose = false);
