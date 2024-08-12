cd318489ba7f852a6349ecb355530ee6ed0b2580
Nicolas-Bessin <nicolasbessin49@gmail.com>
2024-08-12 15:25:42 +0200

    Adapting the heuristics for custom order of exploration and required / forbidden edges

--- src/heuristics.cpp
+++ src/heuristics.cpp
@@ -66,7 +66,7 @@ std::vector<Route> greedy_heuristic(const Instance& instance) {
     return routes;
 }
 
-std::vector<Route> greedy_heuristic_alphas(const Instance& instance) {
+std::vector<Route> greedy_heuristic_bpnode(const Instance& instance, const BPNode& node, std::vector<int> vehicle_order) {
     using std::vector;
     using std::unique_ptr;
     using std::cout, std::endl;
@@ -74,36 +74,27 @@ std::vector<Route> greedy_heuristic_alphas(const Instance& instance) {
     vector<Route> routes;
     int n_interventions = instance.number_interventions;
     int n_vehicles = instance.vehicles.size();
-    // Alphas : contains 0 for interventions not yet covered, and a big value for interventions already covered
-    vector<double> alphas(n_interventions, 0);
-    // Still count the number of covered interventions
     vector<int> covered(n_interventions, 0);
-    
-    // Randomly shuffle the vehicles
-    std::random_device rd;
-    std::mt19937 g(rd());
-    vector<int> vehicle_indices(n_vehicles);
-    // Fill the vector with the indices of the vehicles
-    std::iota(vehicle_indices.begin(), vehicle_indices.end(), 0);
-    // Shuffle the indices
-    std::shuffle(vehicle_indices.begin(), vehicle_indices.end(), g);
-    
-    // // Order the vehicles by increasing number of interventions available
-    // vector<int> vehicle_indices(n_vehicles);
-    // std::iota(vehicle_indices.begin(), vehicle_indices.end(), 0);
-    // std::sort(vehicle_indices.begin(), vehicle_indices.end(), [&instance](int i, int j) {
-    //     return instance.vehicles[i].interventions.size() < instance.vehicles[j].interventions.size();
-    // });
-    
-    // Enumerate through all vehicles : generate a route that maximizes its benefit
-    // Among the interventions that are not yet covered
-    for (const int v_id: vehicle_indices) {
+
+    vector<int> order = vehicle_order;
+    // If no order is given, order the vehicles by increasing number of interventions available
+    if (order.size() == 0) {
+        order.resize(n_vehicles);
+        std::iota(order.begin(), order.end(), 0);
+        std::sort(order.begin(), order.end(), [&instance](int i, int j) {
+            return instance.vehicles[i].interventions.size() < instance.vehicles[j].interventions.size();
+        });
+    }
+
+    for (const int v_id: order) {
         // Find the vehicle
         const Vehicle& vehicle = instance.vehicles[v_id];
+        // Transform the current vehicle to only consider the interventions that are not yet covered
+        const Vehicle& v = vehicle_mask(vehicle, covered);
         // Create a pricing problem for the vehicle
-        unique_ptr<Problem> pricing_problem = create_pricing_instance(instance, vehicle);
+        unique_ptr<Problem> pricing_problem = create_pricing_instance(instance, v, node.upper_bound_cuts, node.lower_bound_cuts);
         // Solve the pricing problem
-        vector<Route> new_routes = solve_pricing_problem(pricing_problem, 1, instance, vehicle);
+        vector<Route> new_routes = solve_pricing_problem(pricing_problem, 1, instance, v);
         // If no route was found, continue to the next vehicle
         if (new_routes.size() == 0) {
             continue;
@@ -113,7 +104,6 @@ std::vector<Route> greedy_heuristic_alphas(const Instance& instance) {
         // Update the covered interventions
         for (int i = 0; i < n_interventions; i++) {
             if (new_route.is_in_route[i] == 1) {
-                alphas[i] = 1e4;
                 covered[i] = 1;
             }
         }
@@ -125,12 +115,7 @@ std::vector<Route> greedy_heuristic_alphas(const Instance& instance) {
     for (int i = 0; i < n_interventions; i++) {
         n_covered += covered[i];
     }
-    cout << "Greedy heuristic covered using alphas " << n_covered << " interventions" << endl;
+    cout << "Greedy heuristic covered " << n_covered << " interventions" << endl;
 
     return routes;
-}
-
-
-std::vector<Route> greedy_heuristic_bpnode(const Instance& instance, const BPNode& node){
-    
 }
--- src/heuristics.h
+++ src/heuristics.h
@@ -14,17 +14,10 @@
 */
 std::vector<Route> greedy_heuristic(const Instance& instance);
 
-/* 
-    Greedy heuristic to generate a set of routes that cover as much interventions as possible
-    Begin with the first vehicle, generate a feasible route that maximizes the benefit.
-    Then, continue with each successive vheicle until either all interventions are covered or no more vehicles are available.
-    This version of the greedy heuristic uses the alphas to penalize interventions already covered.
-*/
-std::vector<Route> greedy_heuristic_alphas(const Instance& instance);
-
 /*
     Greedy heuristic to generate a set of routes that cover as much interventions as possible at a given BPNode
-    Begin with the first vehicle, generate a feasible route that maximizes the benefit.
+    Begin with the first vehicle in the given order, generate a feasible route that maximizes the benefit.
     Then, continue with each successive vheicle until either all interventions are covered or no more vehicles are available.
+    If no vehicle order is given, the vehicles are explored from the one who can do the least interventions to the one who can do the most.
 */
-std::vector<Route> greedy_heuristic_bpnode(const Instance& instance, const BPNode& node);
+std::vector<Route> greedy_heuristic_bpnode(const Instance& instance, const BPNode& node = {}, std::vector<int> vehicle_order = {});
